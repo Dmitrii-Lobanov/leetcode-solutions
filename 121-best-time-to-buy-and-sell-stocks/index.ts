9f8b505fa2956869d03497a38aceb9659942bbf3
Dmitrii <dmitriilobanov3@gmail.com>
2026-04-01 13:34:04 -0500

    121: Best time to Buy and Sell Stocks

---
+++ 121-best-time-to-buy-and-sell-stocks/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+export const maxProfit = (prices: number[]): number => {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else {
+      const profit = prices[i] - minPrice;
+      maxProfit = Math.max(maxProfit, profit);
+    }
+  }
+
+  return maxProfit;
+};
